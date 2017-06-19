9d5fc7004ba57d47b25daf98a3bdfecfe2adac56
leikkisa <sallymaki@gmail.com>
2017-06-19 14:48:54 -0700

    Finish part 1

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,8 +1,30 @@
 const weekday = (date) => {
-  let days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  if(!(date instanceof Date)){
+    throw new Error("Please use a valid date.")
+  }
+  let days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat' ]
+  return days[date.getDay()]
 }
 
-// 10: A test using expected/valid inputs for the weekday() function is written in tests.js.
-// 10: A test using unexpected/invalid inputs for the weekday() function is written in tests.js.
-// 30: Correct implementation of the weekday() is defined in functions.js.
-// 10: Tests for weekday() are passing.
+const snippet = (string, maxlength) => {
+  if((typeof string !== 'string') || !(maxlength >>> 0 === parseFloat(maxlength))) {
+    throw new Error("Please use a valid string and max length.")
+  }
+  return string.slice(0,maxlength) + (string.length > maxlength ? '...' : '')
+}
+
+const numProps = (object) => {
+  if(typeof object !== 'object') {
+    throw new Error("Please use a valid object.")
+  }
+  return Object.keys(object).length
+}
+
+const filterBetween = (array, min, max) => {
+  if (!Array.isArray(array) || array.some(isNaN) || isNaN(min) || isNaN(max)) {
+    throw new Error("Please enter an array of numbers, and min and max numbers.")
+  }
+  return array.filter(num => num >= min && num <= max)
+}
+
+module.exports = { weekday, snippet, numProps, filterBetween }
---
+++ part-1/tests.spec.js
@@ -0,0 +1,70 @@
+var { weekday, snippet, numProps, filterBetween } = require('./functions')
+
+describe('weekday()', function() {
+
+  it('returns Monday', function() {
+    let date = new Date(2017, 5, 19) // June 19, 2017
+    expect(weekday(date)).toEqual('Mon');
+  })
+
+  it('returns Thursday', function() {
+    let date = new Date(2017, 5, 19) // June 19, 2017
+    expect(weekday(new Date(2017, 5, 15))).toEqual('Thu')
+  });
+
+  it('Throws an error if not passed a valid date', function() {
+    expect( function(){ weekday('hello') } ).toThrow(new Error("Please use a valid date."))
+  })
+
+})
+
+describe('snippet()', function() {
+
+  it('returns snippet with ellipses', function() {
+    expect(snippet("For the following exercises,", 10)).toEqual("For the fo...");
+  })
+
+  it('returns full expression without ellipses if less than max length', function() {
+    expect(snippet("Hello, world!", 20)).toEqual("Hello, world!")
+  })
+
+  it('Throws an error if not passed a valid snippet', function() {
+    expect( function(){ snippet("hello", "goodbye") } ).toThrow(new Error("Please use a valid string and max length."))
+  })
+
+})
+
+describe('numProps()', function() {
+
+  it('returns the number of properties of an object', function() {
+    let friend = {
+      name: 'Dominique',
+      age: 30,
+      phone: '555-555-5555'
+    }
+    expect(numProps(friend)).toEqual(3)
+  })
+
+  it('returns number of properties of an empty object', function() {
+    expect(numProps({})).toEqual(0)
+  })
+
+  it('Throws an error if not passed a valid object', function() {
+    expect( function(){ numProps(42) } ).toThrow(new Error("Please use a valid object."))
+  })
+
+});
+
+describe('filterBetween()', function() {
+
+  it('returns only parts of the array within the range defined by min and max', function() {
+    let arr = [ 1, 2, 3, 4, 5, 6, 7 ]
+    expect(filterBetween(arr, 3, 6)).toEqual([ 3, 4, 5, 6 ])
+    expect(filterBetween(arr, 5, 10)).toEqual([ 5, 6, 7 ])
+  })
+
+  it('Throws an error if passed invalid inputs', function() {
+    expect( function(){ filterBetween([ 100, 200, 300 ], 'hello', 'goodbye') } ).toThrow(new Error("Please enter an array of numbers, and min and max numbers."))
+  })
+
+});
