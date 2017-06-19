4a1df621146fcc0efc177a8a8b520abb4b0801f4
Sally Maki <sally@leikkisa.local>
2017-06-19 10:26:44 -0700

    Create part 1 files

---
+++ part-1/functions.js
@@ -0,0 +1,8 @@
+const weekday = (date) => {
+  let days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+}
+
+// 10: A test using expected/valid inputs for the weekday() function is written in tests.js.
+// 10: A test using unexpected/invalid inputs for the weekday() function is written in tests.js.
+// 30: Correct implementation of the weekday() is defined in functions.js.
+// 10: Tests for weekday() are passing.
